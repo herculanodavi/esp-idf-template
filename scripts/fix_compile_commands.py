48695f399dddd00429972cdf96070e2309884b5d
apre <herculanodavi@gmail.com>
2022-12-19 17:55:52 -0300

    feat: static analysis works

--- scripts/fix_compile_commands.py
+++ scripts/fix_compile_commands.py
@@ -2,6 +2,7 @@ import os
 import json
 import subprocess
 import sys
+import shutil 
 
 
 IDF_PATH = os.environ.get('IDF_PATH')
@@ -13,19 +14,24 @@ def fix(line):
 
 
 def fix_compile_commands(filename):
-    with open(filename) as f:
+    clang_commands_file = os.path.join(os.path.dirname(filename), 'clang-tidy-commands.json')
+    shutil.copyfile(filename, clang_commands_file)
+
+    with open(clang_commands_file) as f:
         commands = json.load(f)
     for comm in commands:
         comm["command"] = " ".join([fix(line) for line in comm["command"].split(" ")])
         comm["command"] = comm["command"].replace("-mlongcalls", "")
         comm["command"] = comm["command"].replace("-fno-tree-switch-conversion", "")
         comm["command"] = comm["command"].replace("-fstrict-volatile-bitfields", "")
-    with open(filename, "w") as f:
+    with open(clang_commands_file, "w") as f:
         json.dump(commands, f)
 
+    return clang_commands_file    
 
-def run_clang_tidy(file):
-    subprocess.run(["clang-tidy", "--config-file", ".clang-tidy", "-p", "build/compile_commands.json", "-fix"] + file)
+
+def run_clang_tidy(commands_file, files):
+    return subprocess.run(["clang-tidy", "--config-file", ".clang-tidy", "-p", commands_file, "-fix"] + files).returncode
 
 
 def run_reconfigure():
@@ -37,9 +43,9 @@ def main():
         raise ValueError("IDF_PATH not found. Run '. ./export.sh'")
     
     run_reconfigure()
-    fix_compile_commands(BUILD_PATH)
-    run_clang_tidy(sys.argv[1:])
+    commands_file = fix_compile_commands(BUILD_PATH)
+    return run_clang_tidy(commands_file, sys.argv[1:])
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
